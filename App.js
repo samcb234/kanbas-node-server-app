4bdd4be18416b7b8f56e117bc15c2be0cde52e2a
samblock <block.sa@northeastern.edu>
2024-04-02 09:35:54 -0400

    finished assignment

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import express from 'express';
 import Hello from './hello';
 import Lab5 from './Lab5';
 import cors from 'cors'
+import CourseRoutes from './Kanbas/courses/routes';
+import ModuleRoutes from './Kanbas/modules/routes';
 const app = express()
 Hello(app)
 Lab5(app)
+CourseRoutes(app)
+ModuleRoutes(app)
 app.use(express.json())
 app.use(cors())
 app.listen(4000)
