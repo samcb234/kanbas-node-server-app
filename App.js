e39eb2b99490272a07168b757b823d35ed86d7b4
samblock <block.sa@northeastern.edu>
2024-04-02 13:33:30 -0400

    fixed stupid bug

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import Hello from './hello';
-import Lab5 from './Lab5';
+import Hello from './Hello.js';
+import Lab5 from './Lab5.js';
 import cors from 'cors'
-import CourseRoutes from './Kanbas/courses/routes';
-import ModuleRoutes from './Kanbas/modules/routes';
+import CourseRoutes from './Kanbas/courses/routes.js';
+import ModuleRoutes from './Kanbas/modules/routes.js';
 const app = express()
+app.use(cors())
+app.use(express.json())
 Hello(app)
 Lab5(app)
 CourseRoutes(app)
 ModuleRoutes(app)
-app.use(express.json())
-app.use(cors())
 app.listen(4000)
