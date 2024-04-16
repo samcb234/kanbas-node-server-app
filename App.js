ad54e9058880563f86cc6f983e4a0863bf8e4120
samblock <block.sa@northeastern.edu>
2024-04-16 11:29:51 -0400

    done up to section 5

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
+import  "dotenv/config";
 import express from 'express';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
 import cors from 'cors'
+import mongoose from 'mongoose';
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from './Kanbas/modules/routes.js';
+import UserRoutes from './Kanbas/Users/routes.js';
+import session from 'express-session';
+mongoose.connect('mongodb://localhost:27017/kanbas',);
 const app = express()
-app.use(cors())
+app.use(cors(
+    {
+        credentials: true,
+        origin: process.env.FRONTEND_URL
+      }
+     
+))
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+      domain: process.env.HTTP_SERVER_DOMAIN,
+    };
+  }
+  app.use(session(sessionOptions));
+  
 app.use(express.json())
 Hello(app)
 Lab5(app)
 CourseRoutes(app)
 ModuleRoutes(app)
+UserRoutes(app)
 app.listen(4000)
